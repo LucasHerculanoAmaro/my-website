247e3ea0614adc993c744d47e79fa9e90b03a236
unknown <lucash.96@hotmail.com>
2025-01-26 23:32:20 -0300

    Correção de configuração de segurança

--- src/app/component/introduction/introduction.component.ts
+++ src/app/component/introduction/introduction.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class IntroductionComponent {
 
+  // Lista de temas
+  themes: string[] = ['secondary', 'principal'];
+  currentThemeIndex: number = 0; // Índice do tema atual
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // Recupera o tema salvo no localStorage ao carregar o componente
+    const savedThemeIndex = localStorage.getItem('selectedThemeIndex');
+    this.currentThemeIndex = savedThemeIndex !== null ? parseInt(savedThemeIndex, 10) : 0;
+    this.applyTheme(this.currentThemeIndex);
+  }
+
+  // Aplica o tema atual ao body e salva no localStorage
+  applyTheme(themeIndex: number): void {
+    document.body.className = this.themes[themeIndex]; // Aplica a classe ao body
+    localStorage.setItem('selectedThemeIndex', themeIndex.toString()); // Salva no localStorage
+  }
+
+  // Alterna para o próximo tema
+  cycleThemes(): void {
+    this.currentThemeIndex = (this.currentThemeIndex + 1) % this.themes.length; // Próximo tema
+    this.applyTheme(this.currentThemeIndex); // Aplica o tema
+  }
+
 }
