1f438cfce6c6358e63ff6b35a63c141387e52efc
unknown <lucash.96@hotmail.com>
2025-01-28 19:21:30 -0300

    Iniciando projeto

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,7 @@
-export { AppServerModule as default } from './app/app.module.server';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-// globalThis.localStorage = {
-//     getItem : () => null,
-//     setItem : () => {},
-//     removeItem : () => {},
-//     clear : () => {}
-// };
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
